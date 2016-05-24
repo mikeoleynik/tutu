79dc5f3256b110928594b3f3a5bd0d05210f55bb
Mike Oleynik <oleynik.mike@yandex.ru>
2016-05-24 14:15:03 +0300

    User controller, welcome

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523114439) do
+ActiveRecord::Schema.define(version: 20160524101430) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -25,8 +25,20 @@ ActiveRecord::Schema.define(version: 20160523114439) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "trains", force: :cascade do |t|
     t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
