cdb64152615edc15ecb1cfa1ea0c3d0eabf69ee8
Mike Oleynik <oleynik.mike@yandex.ru>
2016-05-26 13:57:03 +0300

    ticket.rb

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :user, :train
+  belongs_to :user 
+  belongs_to :train
   belongs_to :primary_station, class_name: 'RailwayStation', foreign_key: :primary_station_id
   belongs_to :terminal_station, class_name: 'RailwayStation', foreign_key: :terminal_station_id
 end
