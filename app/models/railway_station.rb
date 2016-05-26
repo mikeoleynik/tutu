99d4966972dcb438f09728e5870d7c9fed84ae46
Mike Oleynik <oleynik.mike@yandex.ru>
2016-05-26 14:05:29 +0300

    current_station_id

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,5 +1,5 @@
 class RailwayStation < ActiveRecord::Base
-  has_many :trains, class_name: 'Train', foreign_key: :trains_id
+  has_many :trains, foreign_key: :current_station_id
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
