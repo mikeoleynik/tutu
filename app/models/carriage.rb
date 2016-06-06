2b9dab7a53224a76a845c2d7d450f1d39e2c3999
Mike Oleynik <oleynik.mike@yandex.ru>
2016-06-06 11:26:31 +0300

    default_scope

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,9 +1,17 @@
 class Carriage < ActiveRecord::Base
-  TYPES = %w(ComfortCarriage EconomyCarriage)
+  TYPES = %w(ComfortCarriage EconomyCarriage SvCarriage SitCarriage)
 
-  validates :number, presence: true
+  validates :number, presence: true, uniqueness: { scope: :train_id }
   validates :type_car, presence: true, inclusion: { in: TYPES }
   validates :up_places, :lower_places, presence: true
   
   belongs_to :train
+
+  after_validation :serial_number
+
+  private
+
+  def serial_number
+    self.number = Carriage.maximum(:number) + 1 # через ключ значение? вывсти во вьюхе || default_scope { order(:number)}
+  end
 end
