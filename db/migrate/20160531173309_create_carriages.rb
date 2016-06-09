b3f29e470a32185ff263eec95617926542ce0524
Mike Oleynik <oleynik.mike@yandex.ru>
2016-06-09 15:15:19 +0300

    schema.rb

--- db/migrate/20160531173309_create_carriages.rb
+++ db/migrate/20160531173309_create_carriages.rb
@@ -1,9 +1,13 @@
 class CreateCarriages < ActiveRecord::Migration
   def change
     create_table :carriages do |t|
+      t.string :type_car
       t.integer :number
       t.integer :up_places
       t.integer :lower_places
+      t.integer :side_up_places
+      t.integer :side_lower_places
+      t.integer :sit_places
       t.belongs_to :train
       t.string :type_car
 
