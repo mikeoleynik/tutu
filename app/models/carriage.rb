a83f6998618cf2186f8ff6714667780f41c34910
Mike Oleynik <oleynik.mike@yandex.ru>
2016-06-09 21:11:57 +0300

    st 100% - 2

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,6 +1,14 @@
 class Carriage < ActiveRecord::Base
   TYPES = %w(ComfortCarriage EconomyCarriage SitCarriage SvCarriage)
   validates :type_car, presence: true, inclusion: { in: TYPES }
-  validates :up_places, :lower_places, presence: true
+  validates :number, uniqueness: { scope: :train_id }
+  
   belongs_to :train
+
+  before_validation :set_number
+
+  private
+  def set_number
+    self.number = train.carriages.maximum(:number) + 1
+  end
 end
--- app/models/economy_carriage.rb
+++ app/models/economy_carriage.rb
@@ -1,3 +1,3 @@
 class EconomyCarriage < Carriage
-  validates :side_up_places, :side_lower_places, presence: true
+  validates :up_places, :lower_places, :side_up_places, :side_lower_places, presence: true
 end
