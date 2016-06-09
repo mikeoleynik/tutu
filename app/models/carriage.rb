a85d450e000a40557260a634b3bf8ab10ff6e61a
Mike Oleynik <oleynik.mike@yandex.ru>
2016-06-09 15:23:21 +0300

    models

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,5 +1,6 @@
 class Carriage < ActiveRecord::Base
-  TYPES = %w(ComfortCarriage EconomyCarriage)
-  validates :type_car, presence: true, :inclusion => { :in => TYPES }
+  TYPES = %w(ComfortCarriage EconomyCarriage SitCarriage SvCarriage)
+  validates :type_car, presence: true, inclusion: { in: TYPES }
+  validates :up_places, :lower_places, presence: true
   belongs_to :train
 end
---
+++ app/models/sv_carriages.rb
@@ -0,0 +1,3 @@
+class SvCarriage < Carriage
+  validates :lower_places, presence: true
+end
