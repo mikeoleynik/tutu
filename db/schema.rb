3e337a1ca0bb353c5bafd2167a4f8e84e491b886
Mike Oleynik <oleynik.mike@yandex.ru>
2016-06-16 15:09:59 +0300

    delete column Route (ids)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610063039) do
+ActiveRecord::Schema.define(version: 20160616120404) do
 
   create_table "carriages", force: :cascade do |t|
     t.string   "type"
