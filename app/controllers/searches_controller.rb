e1a9b188db2b5cc08db5e0cf367e44e1a13635a9
Mike Oleynik <oleynik.mike@yandex.ru>
2016-06-21 10:12:29 +0300

    search 4.2 100%

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,10 +3,11 @@ class SearchesController < ApplicationController
   end
 
   def new
+
   end
 
   def create
-    @trains = Search.search_trains(params[:station_first], params[:station_last])
+    @trains = Search.search_trains(params[:primary_station_id], params[:terminal_station_id])
     render :show
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,8 @@
 class Search
-  def self.search_trains(station_first, station_last)
-    if station_first != station_last
-      routes = Route.joins(:railsway_stations).where(railsway_stations: { id: station_first }) &
-               Route.joins(:railsway_stations).where(railsway_stations: { id: station_last })
+  def self.search_trains(primary_station, terminal_station)
+    if primary_station != terminal_station
+      routes = Route.joins(:railway_stations).where(railway_stations: { id: primary_station }) &
+               Route.joins(:railway_stations).where(railway_stations: { id: terminal_station })
       Train.joins(:route).where(route: routes)
     end
   end
