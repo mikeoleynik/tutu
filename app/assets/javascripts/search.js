cb5203ddbebf73d9f3a1d36c68a3739400b21f72
Mike Oleynik <oleynik.mike@yandex.ru>
2016-07-04 09:11:16 +0300

    search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
   $('.search_form').submit(function() {
-    var primary_station;
-    var terminal_station;
+    
+    var primaryStation = $(this).find('#primary_station_id');
+    var terminalStation = $(this).find('#terminal_station_id');
 
-    primary_station = $(this).find('#primary_station_id');
-    terminal_station = $(this).find('#terminal_station_id');
-
-    if ( primary_station.val() == terminal_station.val() ) {
+    if ( primaryStation.val() == terminalStation.val() ) {
       alert('Выберите конечную станцию');
       return false;
     }
